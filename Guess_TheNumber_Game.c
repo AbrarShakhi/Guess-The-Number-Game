271fb57436d0131b938a418a3dfa0094d06da2e2
Md. Shakhiul Abrar <114344002+AbrarShakhi@users.noreply.github.com>
2022-12-13 19:56:34 +0600

    Add files via upload

---
+++ Guess_TheNumber_Game.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main() {
+    int gen_num, guess, number_of_guesses=1;
+    srand(time(0));
+    gen_num=rand()%100 + 1;
+printf("%d", gen_num);
+    printf("Computer has genarated a number from 1 to 100.\nTry to guess that number.\n");
+    do {
+        printf("Is it : ");
+        scanf("%d", &guess);
+        
+        if(guess>gen_num)
+            printf("Guess a lower number please!\n");
+        else if(guess<gen_num)
+            printf("Guess a higher number please!\n");
+        else
+            printf("You guessed it in %d attempts\n", number_of_guesses);
+            
+        number_of_guesses++;
+    }while(guess!=gen_num);
+    return 0;
+}
